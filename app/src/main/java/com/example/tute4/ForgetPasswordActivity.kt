ebcb90527d8f6ce330ab709a0b447f0f7c9afeb0
Shanchika <shanchika24@gmail.com>
2025-03-28 01:53:13 +0530

    initial page1 add20

--- app/src/main/java/com/example/tute4/ForgetPasswordActivity.kt
+++ app/src/main/java/com/example/tute4/ForgetPasswordActivity.kt
@@ -1,4 +1,23 @@
 package com.example.tute4
 
-class ForgetPasswordActivity {
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class ForgetPasswordActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_forgetpassword)
+
+        // Finding the Submit button
+        val submitButton: Button = findViewById(R.id.button54)
+
+        // Navigate to Sign In activity on Submit button click
+        submitButton.setOnClickListener {
+            val intent = Intent(this, SigninActivity::class.java)
+            startActivity(intent)
+        }
+    }
 }
